06df14c2b1952c18c6f7c56db68e76a920bed71f
iteye <iteyelmp@gmail.com>
2025-11-04 19:00:32 +0800

    remove code

--- src/IOptimismPortal2.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.17;
-
-interface IOptimismPortal2 {
-    /// @notice Mint a specific amount of L2 native token to an address.
-    function mintTransaction(address _to, uint256 _value) external;
-}
